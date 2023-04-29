df56adee07108902a5a0ef9f8567f21f5b3df1ee
oguzhanoxel <oguzhanoksel@hotmail.com>
2023-04-29 17:45:53 +0300

    templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Oğuzhan Öksel',
+        'title': 'An Title',
+        'content': 'an content about nothing',
+        'date_posted': 'April 29, 2023',
+    },
+    {
+        'author': 'Elfo',
+        'title': 'Hi I\'m Elfo',
+        'content': 'an content about Elfo',
+        'date_posted': 'November 19, 2022',
+    },
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<p>Hello, Home Page</p>"
+    return render_template('home.html', posts=posts, title='Home')
 
 
 @app.route("/about")
 def about():
-    return "<p>About Page</p>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
